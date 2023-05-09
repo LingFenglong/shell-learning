e969a88decd249796d941577f3b5b79235436e8c
LingFenglong <2808021998@qq.com>
2023-05-09 19:42:26 +0800

    Upsate myShellTest.sh

--- myShellTest.sh
+++ myShellTest.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-for((i=0;i<$#;i++)); do
-    echo $i
-done
+# for((i=0;i<$#;i++)); do
+#     echo $i
+# done
 
-echo "$1"
-echo "$2"
-echo "$3"
+# echo "$1"
+# echo "$2"
+# echo "$3"
+
+A=12
+B=23
+C='$A $B'
+let X=$A+$B
+echo $C A $X
